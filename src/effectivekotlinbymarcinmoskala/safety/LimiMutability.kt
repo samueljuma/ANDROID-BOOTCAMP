cf4c49212ddf28468ea2b6adacdb6361885498c1
samueljuma <samueljuma1024@gmail.com>
2026-03-26 13:02:07 +0300

    1-limit-mutability

--- src/effectivekotlinbymarcinmoskala/safety/LimiMutability.kt
+++ src/effectivekotlinbymarcinmoskala/safety/LimiMutability.kt
@@ -1,10 +1,27 @@
 package effectivekotlinbymarcinmoskala.safety
 
+class InsufficientBalance: Exception()
+
 class BankAccount{
     var balance = 0.0
         private set
 
     fun deposit(amount:Double){
-
+        balance += amount
     }
+
+    @Throws(InsufficientBalance::class)
+    fun withdraw(amount: Double){
+        if(balance < amount) throw InsufficientBalance()
+        balance -= amount
+    }
+}
+
+fun main(){
+    val account = BankAccount()
+    println(account.balance)
+    account.deposit(100.0)
+    println(account.balance)
+    account.withdraw(10.0)
+    println(account.balance)
 }
