1c9e4d4e29e3e0e1d76df13feea5e8f301fb4c78
samueljuma <samueljuma1024@gmail.com>
2026-04-01 09:55:29 +0300

    platform types behavior

--- src/effectivekotlinbymarcinmoskala/safety/EliminatePlatformTypesASAP.kt
+++ src/effectivekotlinbymarcinmoskala/safety/EliminatePlatformTypesASAP.kt
@@ -1,2 +1,30 @@
 package effectivekotlinbymarcinmoskala.safety
 
+fun platformType(){
+    val value = JavaClass().value
+    println(value.length) // NPE
+}
+
+fun statedType(){
+    val value: String = JavaClass().value // NPE
+    println(value.length)
+}
+
+interface UserRepo{
+    fun getUserName() = JavaClass().value
+}
+
+class RepoImpl: UserRepo {
+    override fun getUserName(): String? {
+        return null
+    }
+}
+
+
+fun main(){
+//    platformType()
+//    statedType()
+    val repo: UserRepo = RepoImpl()
+    val text: String = repo.getUserName()
+    print("User name length is ${text.length}")
+}
---
+++ src/effectivekotlinbymarcinmoskala/safety/JavaClass.java
@@ -0,0 +1,8 @@
+package effectivekotlinbymarcinmoskala.safety;
+
+
+public class JavaClass {
+    public String getValue(){
+        return null;
+    }
+}
